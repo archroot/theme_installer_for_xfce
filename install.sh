3c2d460a48ec9070139a650af8bd4bbc8444c9e0
archroot <41123605+archroot@users.noreply.github.com>
2018-07-12 13:46:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ aurman -S osx-arc-shadow xcursor-breeze-serie-obsidian papirus-maia-icon-theme-g
 
 echo 'Качаем и устанавливаем настройки Xfce'
 cd ~
-wget https://github.com/archroot/theme_installer_for_xfce/raw/master/files/.config.tar.gz
+wget https://github.com/archroot/theme_installer_for_xfcex4/raw/master/files/.config.tar.gz
 tar -xzf .config.tar.gz -C ~
 
 echo 'Ставим лого ArchLinux в меню'
-wget https://github.com/archroot/theme_installer_for_xfce/raw/master/files/archlinux_logo.png
+wget https://github.com/archroot/theme_installer_for_xfce4/raw/master/files/archlinux_logo.png
 sudo mv -f ~/archlinux_logo.png /usr/share/pixmaps/archlinux_logo.png
 
 echo 'Ставим обои на рабочий стол'
-wget https://github.com/archroot/theme_installer_for_xfce/raw/master/files/Deepin_Linux_Manjaro.jpg
+wget https://github.com/archroot/theme_installer_for_xfce4/raw/master/files/Deepin_Linux_Manjaro.jpg
 sudo mv -f ~/Deepin_Linux_Manjaro.jpg /usr/share/backgrounds/xfce/
 
 echo 'Установка завершена!'
